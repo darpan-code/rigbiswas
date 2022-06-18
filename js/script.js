717a77f693d03599de1c7cff56022cd05bbb0554
Darpan Chakraborty <darpanchakraborty2@gmail.com>
2022-06-18 19:20:55 +0530

    navbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#nav-btn").click(function(){
+        $("#nav-btn-icon").toggleClass("fa-xmark fa-bars");
+        $("#nav-menu2").toggleClass("nav-menu nav-menu2");
+    });
+});
