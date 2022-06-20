9f4d3bb818267f5b507c6fe16ea395e21de066f1
Darpan Chakraborty <darpanchakraborty2@gmail.com>
2022-06-20 19:02:32 +0530

    counter section

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
         $("#nav-btn-icon").toggleClass("fa-xmark fa-bars");
         $("#nav-menu2").toggleClass("nav-menu nav-menu2");
     });
+
+    $('.count').counterUp({
+        delay: 1,
+        time: 500
+    });
 });
